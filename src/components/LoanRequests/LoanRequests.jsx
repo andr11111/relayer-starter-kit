05e9542e247fce9e75036514eb24eef4d61ca0d2
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-13 13:55:12 -0700

    feat: add LoanRequests component

---
+++ src/components/LoanRequests/LoanRequests.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import "./LoanRequests.css";
+
+class LoanRequests extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <th>Principal</th>
+                    <th>Interest Rate</th>
+                    <th>Term Length</th>
+                    <th>Collateral</th>
+                    <th>Expiration</th>
+                    <th />
+                </thead>
+                <tbody />
+            </table>
+        );
+    }
+}
+
+export default App;
