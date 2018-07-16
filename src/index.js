215afc72554fa603382dfc921405aeaf6150576c
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-16 12:13:02 -0700

    style: add whitespace

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+import App from "./App";
+
+import "./index.css";
 
 ReactDOM.render(
     <BrowserRouter>
