544fdad7f1e3078c27b037003b07b79b38c11486
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-16 12:15:06 -0700

    feat: add bootstrap navbar to header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 import "./Header.css";
 
 class Header extends Component {
     render() {
-        return <div>Dharma Relayer Starter Kit</div>;
+        return (
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>Dharma Relayer</Navbar.Brand>
+                </Navbar.Header>
+
+                <Nav>
+                    <LinkContainer to="/">
+                        <NavItem eventKey={1}>Browse</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/create">
+                        <NavItem eventKey={2}>Create</NavItem>
+                    </LinkContainer>
+                </Nav>
+            </Navbar>
+        );
     }
 }
 
