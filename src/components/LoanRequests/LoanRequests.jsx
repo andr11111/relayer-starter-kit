f8c60d52a734fdfefb53d1decb5bc91367bbdcfb
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-17 18:28:16 -0700

    feat: load loan requests from disk and call getTerms to render in UI

--- src/components/LoanRequests/LoanRequests.jsx
+++ src/components/LoanRequests/LoanRequests.jsx
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
 import { Table } from "react-bootstrap";
+import Dharma from "@dharmaprotocol/dharma.js";
 
+import Api from "../../services/api";
 import FillButton from "../FillButton/FillButton";
 
 import "./LoanRequests.css";
 
 class LoanRequests extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            requests: [],
+        };
+    }
+
+    componentDidMount() {
+        const api = new Api();
+
+        const { dharma } = this.props;
+
+        const { LoanRequest } = Dharma.Types;
+
+        api.get("loanRequests")
+            .then((data) => {
+                const promises = data.map((datum) => {
+                    return LoanRequest.load(dharma, datum);
+                });
+
+                Promise.all(promises).then((loanRequests) => {
+                    const requests = loanRequests.map((request) => request.getTerms());
+                    this.setState({ requests });
+                });
+            })
+            .catch((error) => console.error(error));
+    }
+
     render() {
-        const { handleFill, requests } = this.props;
+        const { handleFill } = this.props;
+
+        const { requests } = this.state;
 
         return (
             <Table striped bordered condensed hover responsive>
@@ -24,20 +57,18 @@ class LoanRequests extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    {requests.map((request) => {
+                    {requests.map((request, i) => {
                         return (
-                            <tr key={request.id}>
-                                <td>{request.principal}</td>
+                            <tr key={i}>
+                                <td>{request.principalAmount}</td>
                                 <td>{request.principalTokenSymbol}</td>
                                 <td>{request.interestRate}</td>
                                 <td>
-                                    {request.termLength} {request.termUnit}
+                                    {request.termDuration} {request.termUnit}
                                 </td>
-                                <td>{request.collateral}</td>
+                                <td>{request.collateralAmount}</td>
                                 <td>{request.collateralTokenSymbol}</td>
-                                <td>
-                                    {request.expirationLength} {request.expirationUnit}
-                                </td>
+                                <td>{request.expiresAt}</td>
                                 <td>
                                     <FillButton
                                         loanRequestId={request.id}
--- src/containers/LoanRequests.jsx
+++ src/containers/LoanRequests.jsx
@@ -1,41 +1,17 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import LoanRequests from "../components/LoanRequests/LoanRequests";
-
-import Api from "../services/api";
 import DharmaConsumer from "../contexts/Dharma/DharmaConsumer";
 
 class LoanRequestsContainer extends Component {
-    constructor(props) {
-        super(props);
-
-        this.handleFill = this.handleFill.bind(this);
-
-        this.state = {
-            requests: [],
-        };
-    }
-
-    componentDidMount() {
-        const api = new Api();
-
-        api.get("loanRequests").then((requests) => {
-            this.setState({requests});
-        }).catch((error) => console.error(error));
-    }
-
     handleFill(loanRequestId) {
         console.log(loanRequestId);
     }
 
     render() {
-        const {requests} = this.state;
-
         return (
             <DharmaConsumer>
-                {(dharma) => (
-                    <LoanRequests requests={requests} handleFill={this.handleFill} dharma={dharma}/>
-                )}
+                {(dharma) => <LoanRequests handleFill={this.handleFill} dharma={dharma} />}
             </DharmaConsumer>
         );
     }
