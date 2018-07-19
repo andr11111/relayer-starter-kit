11ac037dcb28d84f611e8b06c2bdad9d483efd4e
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-18 18:13:08 -0700

    feat: load loan request in componet did mount

--- src/components/LoanRequest/LoanRequest.jsx
+++ src/components/LoanRequest/LoanRequest.jsx
@@ -2,6 +2,7 @@ import Dharma from "@dharmaprotocol/dharma.js";
 import React, { Component } from "react";
 import * as moment from "moment";
 
+import Api from "../../services/api";
 import FillButton from "../FillButton/FillButton";
 
 class LoanRequest extends Component {
@@ -11,33 +12,35 @@ class LoanRequest extends Component {
         this.handleFill = this.handleFill.bind(this);
     }
 
+    componentDidMount() {
+        const { LoanRequest } = Dharma.Types;
+
+        const { dharma, id } = this.props;
+
+        const api = new Api();
+
+        api.get(`loanRequests/${id}`).then(async (loanRequestData) => {
+            const loanRequest = await LoanRequest.load(dharma, loanRequestData);
+
+            this.setState({ loanRequest });
+        });
+    }
+
     isExpired(unixTimestamp) {
         return moment.unix(unixTimestamp).isBefore();
     }
 
-    handleFill(loanRequestId) {
-        const { dharma } = this.props;
-
-        const { LoanRequest } = Dharma.Types;
-
-        const api = new Api();
-
-        api.get(`loanRequests/${loanRequestId}`).then(async (loanRequestData) => {
-            const loanRequest = await LoanRequest.load(dharma, loanRequestData);
-            const creditorAddress = await loanRequest.getCurrentUser();
-
-            await loanRequest.fill(creditorAddress);
-        });
+    async handleFill() {
+        await this.state.loanRequest.fill();
     }
 
     render() {
-        const { request } = this.props;
+        const { loanRequest } = this.state;
 
         return (
             <div>
                 <FillButton
-                    loanRequestId={request.id}
-                    disabled={this.isExpired(request.expiresAt)}
+                    disabled={this.isExpired(loanRequest.expiresAt)}
                     handleFill={this.handleFill}
                 />
             </div>
