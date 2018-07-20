68ed6cd916c471e1a6f015cf47ec422d413c60e7
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-20 11:22:55 -0700

    feat: on completion, highlight row in query params

--- src/containers/RequestLoanForm.jsx
+++ src/containers/RequestLoanForm.jsx
@@ -11,7 +11,7 @@ class RequestLoanFormContainer extends Component {
     }
 
     onCompletion() {
-        this.props.history.push("/");
+        this.props.history.push("/?highlightRow=true");
     }
 
     render() {
