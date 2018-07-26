cb8c3955bdc92f4e4375eb8381c4dccc008800e3
Graeme <graemelearn@protonmail.com>
2018-07-26 11:32:51 -0700

    refactor: remove unnecessary getTimeUnits function

--- src/components/RequestLoanForm/TimeUnitSelect/TimeUnitSelect.jsx
+++ src/components/RequestLoanForm/TimeUnitSelect/TimeUnitSelect.jsx
@@ -1,36 +1,32 @@
 import React, { Component } from "react";
 import { FormControl } from "react-bootstrap";
 
-class TimeUnitSelect extends Component {
-    getTimeUnits() {
-        return [
-            {
-                label: "Hour",
-                value: "hours",
-            },
-            {
-                label: "Day",
-                value: "days",
-            },
-            {
-                label: "Week",
-                value: "weeks",
-            },
-            {
-                label: "Month",
-                value: "months",
-            },
-            {
-                label: "Year",
-                value: "years",
-            },
-        ];
-    }
+const TIME_UNITS = [
+    {
+        label: "Hour",
+        value: "hours",
+    },
+    {
+        label: "Day",
+        value: "days",
+    },
+    {
+        label: "Week",
+        value: "weeks",
+    },
+    {
+        label: "Month",
+        value: "months",
+    },
+    {
+        label: "Year",
+        value: "years",
+    },
+];
 
+class TimeUnitSelect extends Component {
     render() {
         const { name, onChange, defaultValue } = this.props;
-
-        const timeUnits = this.getTimeUnits();
 
         return (
             <FormControl
@@ -39,7 +35,7 @@ class TimeUnitSelect extends Component {
                 componentClass="select"
                 placeholder="select"
                 defaultValue={defaultValue}>
-                {timeUnits.map((unit) => (
+                {TIME_UNITS.map((unit) => (
                     <option key={unit.value} value={unit.value}>
                         {unit.label}
                     </option>
